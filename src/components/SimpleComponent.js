c668c9cbb76e4c35d5d2cd5d7d775f051adcfb2c
okorkov <alex.okorkov@gmail.com>
2021-03-19 20:59:11 -0700

    commit tree bae358e7af6d4da177137318ea95a4b3972810ee

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
-// Code SimpleComponent Here
+import React from 'react';
+
+class SimpleComponent extends React.Component {
+
+  state = {
+    mood: 'happy'
+  }
+
+  render() {
+    return (
+      <div onClick={this.changeMood} >{this.state.mood}</div>
+    )
+  }
+
+  changeMood = (e) => {
+    this.setState({
+      mood: 'sad'
+    })
+  }
+}
+
+export default SimpleComponent;
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,13 @@
-// Code SimplerComponent Here
+import React from 'react';
+
+const changeMood = (e) => {
+  e.target.innerHTML = 'Not So Happy ANYMORE ;(((((((('
+}
+
+const SimplerComponent = (props) => {
+  return (
+    <div onClick={props.handleClick}>"I am just happy"</div>
+  )
+}
+
+export default SimplerComponent;
